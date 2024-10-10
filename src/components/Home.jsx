bab8da39e768dfa928c146ac65a6bc84649daf0c
Haneen <htsalhagbani@gmail.com>
2024-10-10 15:12:44 +0300

    add exam solution 3!

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,8 +11,9 @@ function Home() {
     <section className="py-8 z-10 font-serif ">
   <div className="flex flex-col md:flex-row items-center max-w-6xl px-6 py-8 mx-auto">
     <div className="w-full md:w-1/2 py-8">
-      <h1 className="text-[#946c5b] md:text-3xl lg:text-7xl max-sm:text-4xl font-semibold leading-none tracking-tighter">
-        Welcome  <br/><span className="text-[gray]"> {localStorage.getItem('username')}  <br/></span>to Book Library
+    <h1 className="text-[#946c5b] text-4xl leading-none tracking-tighter">To support reading and education by bridging the gap in book accessibility.</h1>
+      <h1 className="text-[#946c5b] md:text-4xl lg:text-7xl max-sm:text-4xl font-semibold ">
+      <h1 className='text-xl mt-8 font-bold'>Welcome  <br/> <span className="text-[gray]"> {localStorage.getItem('username')}  </span>to Book Library</h1>  
       </h1>
     </div>
     <div className="w-full flex justify-center items-center md:w-1/2 py-8  ">
